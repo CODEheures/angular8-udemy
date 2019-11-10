94682837fa8ebff4cef4d858ae03bd056ca5085e
CODEheures <webmaster@codeheures.fr>
2019-11-10 15:34:29 +0100

    [INIT] initialyze commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from '../types/pokemon';
+import { POKEMONS } from '../mocks/pokemons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon-app';
+  private pokemons: Pokemon[];
+
+  ngOnInit(): void {
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon): void {
+    alert('Ce pokemon se nomme ' + pokemon.name);
+  }
 }
